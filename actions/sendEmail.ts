1f3a1da3a8ca89e4e25abe14a10b41811a80bea8
NagiAk <lemchiyakh@gmail.com>
2024-07-19 15:08:40 +0100

    change

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -26,7 +26,7 @@ export const sendEmail = async (formData: FormData) => {
   let data;
   try {
     data = await resend.emails.send({
-      from: "Contact Form <onboarding@resend.dev>",
+      from: "Portfolio Email <onboarding@resend.dev>",
       to: "lemchiyakh@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail,
