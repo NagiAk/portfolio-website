2d187a8a06299d95f1dea006fa2d9fac15cb5ee2
NagiAk <lemchiyakh@gmail.com>
2024-12-07 12:16:14 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
+import Studies from "@/components/studies";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <About />
       <Projects />
       <Skills />
+      <Studies />
       <Experience />
       <Contact />
     </main>
